1e4e6be6fbbf9ea3e41f3f59098679ed20aabc13
lyudongliang <dongliang040@sina.com>
2021-01-25 15:03:06 +0800

    add file

---
+++ scripts/segment_scene.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+import rospy
+import numpy as np
+from sensor_msgs.msg import PointCloud2
+from point_cloud_segmentation.srv import SegmentScene
+
+
+def handle_scene(req):
+    print('handle scene.')
+    print('cloud width: %i, cloud height: %i'%(req.cloud_in.width, req.cloud_in.height))
+    pass
+
+
+def scene_server():
+    rospy.init_node('segment_scene_server_node', anonymous=True)
+    
+    rospy.Service('segment_scene_server', SegmentScene, handle_scene)
+    rospy.loginfo("Ready to segment scene.")
+    rospy.spin()
+
+
+
+if __name__ == '__main__':
+    scene_server()
